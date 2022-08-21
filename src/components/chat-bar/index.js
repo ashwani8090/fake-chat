29eb87b371020bb1e5e4d1bbc750068ee26e2791
ashwani <ashwani.singh@daffodilsw.com>
2022-08-21 21:43:03 +0530

    chat box

--- src/components/chat-bar/index.js
+++ src/components/chat-bar/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import emoji from '../../assets/images/happy-face.png';
 import { SendOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+import './styles.css';
 
-const ChatBar = ({ onPress = () => { } }) => {
+const ChatBar = ({ onPress = () => { }, customStyle = {} }) => {
     const [message, setMessage] = React.useState('');
+    const emojiFace = <img height={20} width={20} src={emoji} alt="emoji" />
 
-    return (<div>
-        <input onChange={(e) => {
-            setMessage(e.target.value);
-        }}
-        type="text"
-        placeholder="Enter your message here" />
-        <div onClick={()=>onPress(message)}>
+    return (<div className='chat-bar' style={{ ...customStyle }}>
+        <Input placeholder='Enter your message here' onChange={setMessage} addonAfter={emojiFace} />
+        <button className='chatbox-send-btn' onClick={() => onPress(message)}>
+            <span className='chat-box-text'>Send</span>
             <SendOutlined />
-        </div>
+        </button>
     </div>
     )
 }
--- src/components/chat-box/index.js
+++ src/components/chat-box/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import ChatBar from '../chat-bar';
 import './styles.css';
 
 export default function ChatBox() {
   return (
-    <div className='chat-box'>index</div>
+    <div className='chat-box'>
+
+
+      <ChatBar customStyle={{
+        position: 'absolute',
+        bottom: 0,
+        left: 2,
+        width: "97%",
+      }} />
+    </div>
   )
 }
